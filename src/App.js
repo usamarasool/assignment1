431a9f7891f8fe7627b8421ba852ef06b887b1eb
muhammad usama <usamarasool117@gmail.com>
2022-11-03 01:23:57 +0500

    yeah

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Random heading
+
+      </h1>
+      <p>
+        jo b dil karay
+      </p>
+      
+    </div>
+  );
+}
+
+export default App;
